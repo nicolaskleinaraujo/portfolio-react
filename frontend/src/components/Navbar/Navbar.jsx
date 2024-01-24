4a07da53099baa9313bb9afe99f9387d3f58aab6
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-01-24 16:25:58 -0300

    finalized the navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-        <ul>
-          <li>Sobre Mim</li>
-          <li>Habilidades</li>
-          <li>Projetos</li>
-          <li>BRLang</li>
-          <li>ENLang</li>
-        </ul>
+      <ul>
+        <li>Sobre Mim</li>
+        <li>Habilidades</li>
+        <li>Projetos</li>
+        <li>BRLang</li>
+        <li>ENLang</li>
+      </ul>
     </nav>
   )
 }
