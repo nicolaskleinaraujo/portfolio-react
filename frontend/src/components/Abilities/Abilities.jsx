6cc0666d1d7e216568b8d3a29a56c1d68fbe597b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-01-25 17:23:47 -0300

    created the abilities section translation to english

--- frontend/src/components/Abilities/Abilities.jsx
+++ frontend/src/components/Abilities/Abilities.jsx
@@ -1,24 +1,48 @@
 // CSS
 import styles from "./Abilities.module.css"
 
-const Abilities = () => {
+const Abilities = ({ lang }) => {
   return (
     <div className={styles.abilities}>
         <h2>Habilidades</h2>
 
         <div className={styles.javascript}>
             <img src="./logos/javascript-logo.png" alt="Logo JavaScript" />
-            <p>JavaScript é a principal linguagem que estudo. Utilizo ela tanto no "React" quanto integrada ao "Node.js" para aplicações back-end. Tenho conhecimento em server e banco de dados.</p>
+            {lang === "br" ? (
+              <>
+                <p>JavaScript é a principal linguagem que estudo. Utilizo ela tanto no "React" quanto integrada ao "Node.js" para aplicações back-end. Tenho conhecimento em server e banco de dados.</p>
+              </>
+            ) : (
+              <>
+                <p>JavaScript is the main language I study to. I use it both on "React" and integrated with "Node.js" for back-end applications. I have knowledge of servers and databases.</p>
+              </>
+            )}
         </div>
 
         <div className={styles.html_css}>
-            <p>HTML e CSS são as principais linguagens que eu utilizo para estruturar e estilizar meus sites. Utilizo ferramentas como o Figma para uma pré-estruturação dos meus sites.</p>
+            {lang === "br" ? (
+              <>
+                <p>HTML e CSS são as principais linguagens que eu utilizo para estruturar e estilizar meus sites. Utilizo ferramentas como o Figma para uma pré-estruturação dos meus sites.</p>
+              </>
+            ) : (
+              <>
+                <p>HTML and CSS are the main languages I use to structure and style my websites. I use tools like Figma to pre-structure my websites.</p>
+              </>
+            )}
             <img src="./logos/html-logo.png" alt="Logo HTML e CSS" />
         </div>
 
         <div className={styles.python}>
             <img src="./logos/python-logo.png" alt="Logo Python" />
-            <p>Python foi a primeira linguagem de programação que eu estudei. Desenvolvo interfaces gráficas e automações de atividades. Tenho conhecimento em bibliotecas como Pandas, PyQT, PyAutoGui e Selenium.</p>
+            {lang === "br" ? (
+              <>
+                <p>Python foi a primeira linguagem de programação que eu estudei. Desenvolvo interfaces gráficas e automações de atividades. Tenho conhecimento em bibliotecas como Pandas, PyQT, PyAutoGui e Selenium.</p>
+              </>
+            ) : (
+              <>
+                <p>Pyhton was the first progamming language I studied. I develop graphical interfaces and activity automations. I have knowledge of libraries such as Pandas, PyQT, PyAutoGui and Selenium.</p>
+              </>
+            )}
         </div>
     </div>
   )
