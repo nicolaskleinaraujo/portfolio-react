64d2884195822551bfde8d9a024cba38f3a3ad43
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-01-25 17:15:01 -0300

    created about english translation

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,7 +1,7 @@
 // CSS
 import styles from "./About.module.css"
 
-const About = () => {
+const About = ({ lang }) => {
   return (
     <div>
         <h2>Sobre Mim</h2>
@@ -19,13 +19,28 @@ const About = () => {
 
         <div className={styles.text}>
             <ul>
-                <li>Meu nome é <strong>Nicolas Klein</strong> e tenho 17 anos.</li>
-                <li>Nasci e <strong>Moro em Mariná</strong>.</li>
-                <li>Possuo <strong>proficiencia em inglês</strong>.</li>
-                <li>Estou <strong>cursando Análise e Desenvolvimento de Sistemas</strong>.</li>
-                <li>A principal linguagem que utilizo é o <strong>JavaScript</strong>.</li>
-                <li>Estudo <strong>desenvolvimento web</strong>, utilizo tecnologias como: <strong>Node.js e React</strong>.</li>
-                <li>Meu principal hobby é jogar, adoro jogos de luta.</li>
+                {lang === "br" ? (
+                    <>
+                        <li>Meu nome é <strong>Nicolas Klein</strong> e tenho 17 anos.</li>
+                        <li>Nasci e <strong>Moro em Mariná</strong>.</li>
+                        <li>Possuo <strong>proficiencia em inglês</strong>.</li>
+                        <li>Estou <strong>cursando Análise e Desenvolvimento de Sistemas</strong>.</li>
+                        <li>A principal linguagem que utilizo é o <strong>JavaScript</strong>.</li>
+                        <li>Estudo <strong>desenvolvimento web</strong>, utilizo tecnologias como: <strong>Node.js e React</strong>.</li>
+                        <li>Meu principal hobby é jogar, adoro jogos de luta.</li>
+                    </>
+                ) : (
+                    <>
+                        <li>My name is <strong>Nicolas Klein</strong> and I am 17 years old.</li>
+                        <li>I was born and <strong>live in Brazil</strong>.</li>
+                        <li>I have <strong>proficiency in english</strong>.</li>
+                        <li>I'm <strong>studying Systems Analysis and Development</strong>.</li>
+                        <li>The main language I use is <strong>JavaScript</strong>.</li>
+                        <li>I study <strong>web development</strong>, I use technologies such as: <strong>Node.js and React</strong>.</li>
+                        <li>My main hobby is to gaming, my favorite genre is fighting.</li>
+                    </>
+                )}
+                
             </ul>
         </div>
     </div>
