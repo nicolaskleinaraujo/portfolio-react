ffcbe3085e5b5a03a2cab61de1e7cd131f246457
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-01-25 16:08:15 -0300

    navbar finalizada

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li>Sobre Mim</li>
-        <li>Habilidades</li>
-        <li>Projetos</li>
-        <li>BRLang</li>
-        <li>ENLang</li>
+        <li className={styles.not_img}>Sobre Mim</li>
+        <li className={styles.not_img}>Habilidades</li>
+        <li className={styles.not_img}>Projetos</li>
+        <li><img src="./others/br-flag.png" alt="Bandeira do Brasil" /></li>
+        <li><img src="./others/us-flag.png" alt="Bandeira do Brasil" /></li>
       </ul>
     </nav>
   )
