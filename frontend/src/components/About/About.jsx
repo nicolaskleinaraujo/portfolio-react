a763efe28072c946ec08f2eff473b5f8f6212049
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-01-29 17:41:26 -0300

    finalized the about component

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -15,42 +15,43 @@ const About = ({ lang }) => {
         )}
         
 
-        <div className={styles.socials}>
-            <img src="./others/my-photo.jpg" alt="Minha foto" />
-            <ul>
-                <li><a href="https://github.com/nicolaskleinaraujo/">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/nicolaskleinaraujo/">Linkedin</a></li>
-                <li><a href="https://wa.me/5544997666224">Whatsapp</a></li>
-                <li>contatonkfa@gmail.com</li>
-                <li>+55 44 99766-6224</li>
-            </ul>
-        </div>
+        <div className={styles.container}>
+            <div className={styles.socials}>
+                <img src="./others/my-photo.jpg" alt="Minha foto" />
+                <ul>
+                    <li><a href="https://github.com/nicolaskleinaraujo/" target="blank">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/nicolaskleinaraujo/" target="blank">Linkedin</a></li>
+                    <li><a href="https://wa.me/5544997666224" target="blank">Whatsapp</a></li>
+                    <li>contatonkfa@gmail.com</li>
+                    <li>+55 44 99766-6224</li>
+                </ul>
+            </div>
 
-        <div className={styles.text}>
-            <ul>
-                {lang === "br" ? (
-                    <>
-                        <li>Meu nome é <strong>Nicolas Klein</strong> e tenho 17 anos.</li>
-                        <li>Nasci e <strong>Moro em Mariná</strong>.</li>
-                        <li>Possuo <strong>proficiencia em inglês</strong>.</li>
-                        <li>Estou <strong>cursando Análise e Desenvolvimento de Sistemas</strong>.</li>
-                        <li>A principal linguagem que utilizo é o <strong>JavaScript</strong>.</li>
-                        <li>Estudo <strong>desenvolvimento web</strong>, utilizo tecnologias como: <strong>Node.js e React</strong>.</li>
-                        <li>Meu principal hobby é jogar, adoro jogos de luta.</li>
-                    </>
-                ) : (
-                    <>
-                        <li>My name is <strong>Nicolas Klein</strong> and I am 17 years old.</li>
-                        <li>I was born and <strong>live in Brazil</strong>.</li>
-                        <li>I have <strong>proficiency in english</strong>.</li>
-                        <li>I'm <strong>studying Systems Analysis and Development</strong>.</li>
-                        <li>The main language I use is <strong>JavaScript</strong>.</li>
-                        <li>I study <strong>web development</strong>, I use technologies such as: <strong>Node.js and React</strong>.</li>
-                        <li>My main hobby is to gaming, my favorite genre is fighting.</li>
-                    </>
-                )}
-                
-            </ul>
+            <div className={styles.text}>
+                <ul>
+                    {lang === "br" ? (
+                        <>
+                            <li>Meu nome é <strong>Nicolas Klein</strong> e tenho 17 anos.</li>
+                            <li>Nasci e <strong>Moro em Mariná</strong>.</li>
+                            <li>Possuo <strong>proficiencia em inglês</strong>.</li>
+                            <li>Estou <strong>cursando Análise e Desenvolvimento de Sistemas</strong>.</li>
+                            <li>A principal linguagem que utilizo é o <strong>JavaScript</strong>.</li>
+                            <li>Estudo <strong>desenvolvimento web</strong>, utilizo tecnologias como: <strong>Node.js e React</strong>.</li>
+                            <li>Meu principal hobby é jogar, adoro jogos de luta.</li>
+                        </>
+                    ) : (
+                        <>
+                            <li>My name is <strong>Nicolas Klein</strong> and I am 17 years old.</li>
+                            <li>I was born and <strong>live in Brazil</strong>.</li>
+                            <li>I have <strong>proficiency in english</strong>.</li>
+                            <li>I'm <strong>studying Systems Analysis and Development</strong>.</li>
+                            <li>The main language I use is <strong>JavaScript</strong>.</li>
+                            <li>I study <strong>web development</strong>, I use technologies such as: <strong>Node.js and React</strong>.</li>
+                            <li>My main hobby is to gaming, my favorite genre is fighting.</li>
+                        </>
+                    )}
+                </ul>
+            </div>
         </div>
     </div>
   )
