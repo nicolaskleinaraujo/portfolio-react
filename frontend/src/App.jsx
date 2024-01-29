41c7bbc9fd5be6d65746ddc73c31493f29420d55
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-01-29 17:08:47 -0300

    projetcs component bootstraped

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Navbar from "./components/Navbar/Navbar"
 import Header from './components/Header/Header'
 import About from './components/About/About'
 import Abilities from './components/Abilities/Abilities'
+import Projects from './components/Projects/Projects'
 import Footer from './components/Footer/Footer'
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
       <Header />
       <About lang={lang} />
       <Abilities lang={lang} />
+      <Projects />
       <Footer lang={lang} />
     </>
   )
---
+++ frontend/src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./Projects.module.css"
+
+const Projects = () => {
+  return (
+    <div>
+        <h1>Projects</h1>
+    </div>
+  )
+}
+
+export default Projects
