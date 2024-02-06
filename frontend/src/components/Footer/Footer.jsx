04e05f12cd8202009942a079cee9fa1a381c6516
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-06 13:58:34 -0300

    add the footer translation

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import styles from "./Footer.module.css"
 
-const Footer = () => {
+const Footer = ({ lang }) => {
   return (
     <footer className={styles.footer}>
-        <p>&copy; 2023 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo/" target="blank">Nicolas Klein</a></p>
+        {lang === "br" ? (
+          <p>&copy; 2023 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo/" target="blank">Nicolas Klein</a></p>
+        ) : (
+          <p>&copy; 2023 - Website developed by <a href="https://github.com/nicolaskleinaraujo/" target="blank">Nicolas Klein</a></p>
+        )}
     </footer>
   )
 }
