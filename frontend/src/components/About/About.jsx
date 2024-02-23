c77ccb50b075c5cee6d5a7aa44ea6ca04f809c9c
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-23 13:57:33 -0300

    corrected all links target from about section

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -28,9 +28,9 @@ const About = forwardRef((props, ref) => {
                     <img src="./others/my-photo.jpg" alt="Minha foto" />
                     <ul>
                         <li><a href="./others/curriculo.pdf" target="_blank">TESTE</a></li>
-                        <li><a href="https://github.com/nicolaskleinaraujo/" target="blank">Github</a></li>
-                        <li><a href="https://www.linkedin.com/in/nicolaskleinaraujo/" target="blank">Linkedin</a></li>
-                        <li><a href="https://wa.me/5544997666224" target="blank">Whatsapp</a></li>
+                        <li><a href="https://github.com/nicolaskleinaraujo/" target="_blank">Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/nicolaskleinaraujo/" target="_blank">Linkedin</a></li>
+                        <li><a href="https://wa.me/5544997666224" target="_blank">Whatsapp</a></li>
                         <li>contatonkfa@gmail.com</li>
                         <li>+55 44 99766-6224</li>
                     </ul>
