3dd69a5eab1529fbf5ff56804fbf019f3426da95
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-23 14:08:24 -0300

    corrected links on the footer section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         {lang === "br" ? (
-          <p>&copy; 2023 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo/" target="blank">Nicolas Klein</a></p>
+          <p>&copy; 2023 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo/" target="_blank">Nicolas Klein</a></p>
         ) : (
-          <p>&copy; 2023 - Website developed by <a href="https://github.com/nicolaskleinaraujo/" target="blank">Nicolas Klein</a></p>
+          <p>&copy; 2023 - Website developed by <a href="https://github.com/nicolaskleinaraujo/" target="_blank">Nicolas Klein</a></p>
         )}
     </footer>
   )
